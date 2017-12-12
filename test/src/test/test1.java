10848ef614c15ac69343447cc406a96c90dcf2ff
Nathan <nathan95220@gmail.com>
2017-12-12 10:34:07 +0100

    powershell execute + read csv

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -3,43 +3,53 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-//import java.io.PrintWriter;
+import java.io.FileReader;
+
 
 public class test1 {
+	
+public static void powerShell() throws IOException{
+	String command = "powershell.exe \"C:\\Users\\natha\\Documents\\test.ps1\" ";
+	Process powerShellProcess = Runtime.getRuntime().exec(command);
+	powerShellProcess.getOutputStream().close();
+}
+
+public static void csvReader(){
+	  String csvFile = "C:\\Users\\natha\\Documents\\get-process_usage_cpu.csv";
+	  FileReader monFichier = null;
+	  BufferedReader tampon = null;
+
+	  try {
+	  monFichier = new FileReader(csvFile);
+	  tampon = new BufferedReader(monFichier);
+
+	  while (true) {
+		  String ligne = tampon.readLine();
+		  if (ligne == null)
+			  break;
+		  System.out.println(ligne);
+	  	} 
+	  } 
+	  catch (IOException exception) 
+	  {
+		  exception.printStackTrace();
+	  } 
+	  finally 
+	  {
+		  try {
+			  tampon.close();
+			  monFichier.close();
+		  }	 
+		  catch(IOException exception1) 
+		  {
+			  exception1.printStackTrace();
+		  }
+	  }
+}
 
  public static void main(String[] args) throws IOException {
-  //PrintWriter writer = new PrintWriter("C:\\Users\\natha\\Documents\\the-file-name.csv");
-  //String command = "powershell.exe  your command";
-  //Getting the version
-  String command = "powershell.exe \"C:\\Users\\natha\\Documents\\test.ps1\" ";
-  // Executing the command
-  Process powerShellProcess = Runtime.getRuntime().exec(command);
-  // Getting the results
-  powerShellProcess.getOutputStream().close();
-  String line;
-  //System.out.println("Standard Output:");
-  BufferedReader stdout = new BufferedReader(new InputStreamReader(
-    powerShellProcess.getInputStream()));
-  while ((line = stdout.readLine()) != null) {
-   System.out.println(line);
-  //Write into file
-   //writer.println(line);
-
-  }
-  stdout.close();
-  //System.out.println("Standard Error:");
-  BufferedReader stderr = new BufferedReader(new InputStreamReader(
-    powerShellProcess.getErrorStream()));
-  while ((line = stderr.readLine()) != null) {
-   //System.out.println(line);
-
-
-  
-  }
-  //writer.close();
-  stderr.close();
- // System.out.println("Done");
-
+	
+	 powerShell();
+	 csvReader();
  }
-
 }
